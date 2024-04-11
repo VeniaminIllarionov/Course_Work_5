de736344c866890f2fdce2244d91e02d39c1aca2
veniamin <venya957@gmail.com>
2024-04-11 13:06:54 +0300

    inital

--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,21 @@ import requests
 
 def get_url(employee_id):
     """Поиск по названию"""
-    params = {
-        "per_page": 20,
-        "employer_id": employee_id,
-        "only_with_salary": True,
-        "area": 113,
-        "only_with_vacancies": True
-    }
-    return requests.get("https://api.hh.ru/vacancies/", params).json()['items']
+    try:
+        params = {
+            "per_page": 20,
+            "employer_id": employee_id,
+            "only_with_salary": True,
+            "area": 113,
+            "only_with_vacancies": True
+        }
+        r = requests.get("https://api.hh.ru/vacancies/", timeout=1, params=params)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error")
+        print(errh.args[0])
+        # Prints the response code
+    return r.json()['items']
 
 
 def get_company(employee_ids):
